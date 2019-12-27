38a2e65d79dc735f6a9f246de406458e62c9a578
z-ach <zachkaufman97@gmail.com>
2019-12-27 11:08:12 -0800

    included more analysis sample code, plots saved as png files

--- Analysis.py
+++ Analysis.py
@@ -2,12 +2,15 @@ from datetime import datetime, timedelta
 
 import pandas as pd
 
+import matplotlib
+
 class Analysis():
 
     def __init__(self, db):
+        matplotlib.use('Agg')
         self.df = db.get_all()
         rn = datetime.now()
-        rn = rn + timedelta(days=-31)
+        rn = rn + timedelta(days=-(31*3))
         limited = self.df.query('_date > {}'.format(rn.timestamp()))
 
         df_week = limited.groupby('weekday')
@@ -21,6 +24,13 @@ class Analysis():
         times = limited.groupby(['weekday', 'hour'], as_index=False)
         mean_times = times['wait_time'].mean()
         print(mean_times.sort_values(by=['wait_time']).head())
+
+        limited = self.df.query('month == 6')
+        day_df = limited.groupby('day').mean()
+        print(day_df)
+        avgs = day_df.plot.bar(y='wait_time', rot=0)
+        fig = avgs.get_figure()
+        fig.savefig("dayplot.png")
 
     class EWMAnalysis():
         pass
